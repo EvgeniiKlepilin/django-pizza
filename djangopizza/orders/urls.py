cb3ae57c7aaf21bc8ec4fb2f8607aa51ed81cf2e
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2020-06-26 19:01:03 +0600

    Added order summary

--- djangopizza/orders/urls.py
+++ djangopizza/orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import add_to_cart, remove_from_cart
+from .views import add_to_cart, remove_from_cart, OrderSummaryView
 
 app_name = "orders"
 
 urlpatterns = [
+    path('order-summary/', OrderSummaryView.as_view(), name="summary"),
     path('add-to-cart/<product_id>', add_to_cart, name="add-to-cart"),
     path('remove-from-cart/<product_id>', remove_from_cart, name="remove-from-cart")
 ]
--- djangopizza/orders/views.py
+++ djangopizza/orders/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 from .models import Order, OrderItem
 from djangopizza.products.models import Product
 
+
+class OrderSummaryView(LoginRequiredMixin, View):
+
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(
+                user=self.request.user,
+                is_ordered=False
+            )
+            context = {
+                'object': order
+            }
+            return render(self.request, 'orders/summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have an active order.")
+            return redirect("products:product-list")
+        
+
+
+@login_required
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     order_item, created = OrderItem.objects.get_or_create(
@@ -27,8 +51,10 @@ def add_to_cart(request, product_id):
         order = Order.objects.create(user=request.user)
         order.items.add(order_item)
         messages.info(request, f"{product.title} has been added to your cart.")
-    return redirect("products:product-list")
+    return redirect(request.META.get('HTTP_REFERER', "orders:summary"))
 
+
+@login_required
 def remove_from_cart(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     order_qs = Order.objects.filter(
@@ -48,9 +74,10 @@ def remove_from_cart(request, product_id):
                 messages.info(request, f"{product.title} has been removed from your cart.")
             else:
                 order_item.quantity -= 1
+                order_item.save()
                 messages.info(request, f"{product.title} quantity has been updated.")
         else:
             messages.info(request, f"{product.title} was not in your cart.")
     else:
         messages.info(request, "You do not have an active order.")
-    return redirect("products:product-list")
+    return redirect(request.META.get('HTTP_REFERER', "orders:summary"))
